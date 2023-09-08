d2ac82086004f88296aad909b1e860bc967ce82a
David Solis <dsolis2@gmail.com>
2023-09-07 21:50:23 -0700

    Added new function and output statements

--- roll-dice/README.md
+++ roll-dice/README.md
@@ -1,3 +1,4 @@
 # python_programs
 Simple program to roll a pair of dice
+The program uses the  
 
--- roll-dice/roll_dice.py
+++ roll-dice/roll_dice.py
@@ -1,17 +1,19 @@
 import random
 
 def roll_dice(x, y):
-    
-    # Generate two random numbers from 1 to 6 that represents 2 dice
+    # Generate two random numbers from 1 to 6 that represents 1 roll of the dice
     return random.randint(x,y)
 
+
+# Roll 2 dices to represent 
 count = 1    
-while count <= 6:
-    
+while count <= 6:  
     result1 = roll_dice(1, 6)
     print('Roll: ', count)
-    print('1st dice: ', result1)
+    print('1st dice: %s' % (result1))
+    dr = (1/6) * 10
+    print('Probability', dr)
     result2 = roll_dice(1, 6)
-    print('2nd dice: ', result2)
+    print('2nd dice:', result2)
     count = count + 1
     
