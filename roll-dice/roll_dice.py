8595458aba5e31ea78bc5c7a63fdb8e2256a3a62
David Solis <dsolis2@gmail.com>
2023-10-03 18:58:47 -0700

    Added comments to the readMe file and the main program

--- roll-dice/roll_dice.py
+++ roll-dice/roll_dice.py
@@ -1,3 +1,10 @@
+'''
+Simple program to roll a pair of dice
+The program uses a function to randomly to roll 6 sided die.
+The function is called twice to simulate 2 dice.
+
+'''
+
 import random
 
 def roll_dice(x, y):
