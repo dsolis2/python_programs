7b7890b9b5b6a26690167ebe16779d0ee7706e34
David Solis <dsolis2@gmail.com>
2024-02-07 10:27:27 -0800

    Initial code push

---
+++ dict-values/print_dict_values.py
@@ -0,0 +1,9 @@
+def print_dict_values(dict):
+  for key, value in dict.items():
+    print(key, value)
+
+# Create a dictionary
+my_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 'Char', 1: 'b'}
+
+# Print the dictionary values
+print_dict_values(my_dict)
