cedd6aeac19565968faee01f548d585fad8bc3b2
David Solis <dsolis2@gmail.com>
2024-02-08 20:38:11 -0800

    Made change in comments

--- max-stack/max_value_stack.py
+++ max-stack/max_value_stack.py
@@ -1,30 +1,4 @@
-"""
-       Given a list of integers: 1,6,3,10,11
- 
-       Your stack should look like:
-       ---
-       11
-       ---
-       10
-       ---
-       3
-       ---
-       6
-       ---
-       1
-       ---
-
-       Implement functions stack.max() which shoudl return max value in the stack. In this
-       example stack.max()=11
-        
-       Also implement stack.pop() which deletes the top value from the stack, in this case 11.
-       Now stack.max()=10
-
-       Also implement stack.push(n) to build the stack.
-       Also implement stack.peek() to print the stack.
-
-"""
-
+#python
 
 # Initial List
 stack = [1,6,3,10,11]
@@ -49,24 +23,11 @@ print("Max value =", val)
 p = stack.pop(0)
 print('Remove top value', p)
 
-# Print max value
+# Print max value of new stack
 val = maxVal(stack)
 print("Max value =", val)
 
-
-#max = maxVal(stack)
-#print("Max value =", max)
-#print(stack)
-#stack.sort(reverse=True) 
-#print("Max value =", max)
-#print("p =", p)
-#stack.append(12)
-#print(stack)
-#print(max(lst))
-#print(lst)
-#print(lst.index(1))
-#print(lst[0])
-
+# print final stack
 for i in range(len(stack)):
    print("---")
    print(stack[i])
