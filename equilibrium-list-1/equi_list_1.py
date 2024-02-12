1492084a6f7880fcbe41c6307e4e937b45bbd4d5
David Solis <dsolis2@gmail.com>
2024-02-12 13:37:21 -0800

    Made small change to the comparison logic

--- equilibrium-list-1/equi_list_1.py
+++ equilibrium-list-1/equi_list_1.py
@@ -4,7 +4,7 @@ import random
 lst = []
 for x in range(10):
     lst.append(random.randint(0,10))
-print(lst)
+print("List:", lst)
 
 # find length of list
 print("Length of list:", len(lst))
@@ -14,20 +14,20 @@ mid = (len(lst)) //2
 
 #find the value of the midpoint
 value = lst[mid]
-print("value", value)
-
+print("Midpoint of the list:", value)
+ 
 # sum left side
-print("left side ", lst[0:mid])
+print("left side of the list:", lst[0:mid])
 left = sum(lst[0:mid])
-print(left)
+print("Sum of the left side:", left)
 
 # sum right side
-print("Right side", lst[mid+1:])
+print("Right side of the list:", lst[mid+1:])
 right = sum(lst[mid+1:])
-print(right)
+print("Sum of the right side:", right)
 
 # compare left and right
 if left < right:
-    print("Right side is smaller")
+    print("Right side is bigger")
 else:
     print("Left side is bigger")    
