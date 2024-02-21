91c1a454c7d4d04cac2b8302b92dbe53719e404e
David Solis <dsolis2@gmail.com>
2024-02-20 16:45:44 -0800

    Initial code push

---
+++ count-loop/count_loop.py
@@ -0,0 +1,22 @@
+# count down functions
+
+
+def count_down(num_1):
+    print("Count down")
+    print("---------")
+    while num_1 > 0 :
+        print(num_1)
+        num_1 = num_1 - 1
+       
+
+# count up
+def count_up(num_2) : 
+    num_2 = 0
+    print("Count up")
+    print("----------")
+    while num_2 < 10:
+        print(num_2)
+        num_2 = num_2 + 1
+       
+
+
---
+++ count-loop/driver.py
@@ -0,0 +1,4 @@
+from count_loop import *
+
+count_up(10)
+count_down(10) 
